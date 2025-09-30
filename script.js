ca127b2172dc17dca80e65acdb116785a5b50b05
sam <sambnm2110@gmail.com>
2025-09-30 22:32:41 +0330

    first commit and push to github

---
+++ script.js
@@ -0,0 +1,18 @@
+// const wrapper = document.getElementById('formWrapper');
+// const toggleBtn = document.getElementById('toggleBtn');
+// const toggleheading = document.getElementById('toggleheading');
+// const toggleText = document.getElementById('toggleText');
+
+// toggleBtn.addEventListener('click', () => {
+//     wrapper.classList.toggle('active');
+
+//     if (wrapper.classList.contains('active')) {
+//         toggleheading.textContent = "Already have an account";
+//         toggleText.textContent = "Login to your account";
+//         toggleBtn.textContent = "Login";
+//     } else {
+//         toggleheading.textContent = "Dont have an account?";
+//         toggleText.textContent = "Sign up to get started";
+//         toggleBtn.textContent = "Sign Up";
+//     }
+// });
